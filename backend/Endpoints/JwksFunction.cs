4c1903e530995b54e62b0a876f5b10615809cbac
AR Along <adolf.rey.along@gmail.com>
2026-04-02 22:28:24 +0800

    Missing imports

--- backend/Endpoints/JwksFunction.cs
+++ backend/Endpoints/JwksFunction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text.Json.Serialization;
+using backend.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
